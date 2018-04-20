36133551e6cfe67e31518bc42af211ed58e76020
dwcbk <38562530+dwcbk@users.noreply.github.com>
2018-04-20 16:09:05 -0400

    Add ContentSearchService method to run regex matcher on a String

--- src/main/java/com/wework/websitesearcher/services/ContentSearchService.java
+++ src/main/java/com/wework/websitesearcher/services/ContentSearchService.java
@@ -1,4 +1,15 @@
 package com.wework.websitesearcher.services;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class ContentSearchService {
+
+    private ContentSearchService() {}
+
+    public static boolean stringContainsTerm(String content, String searchTerm) {
+        if (StringUtils.isEmpty(content) || StringUtils.isEmpty(searchTerm)) {
+            return false;
+        }
+        return content.matches(searchTerm);
+    }
 }
